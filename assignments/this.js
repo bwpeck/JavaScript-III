995f1035ecd77c9ce79c5ac3bbe8f5e3078d9607
bwpeck <48737515+bwpeck@users.noreply.github.com>
2019-04-10 19:35:36 -0400

    directions exist for a reason

--- assignments/this.js
+++ assignments/this.js
@@ -12,13 +12,37 @@
 // Principle 1
 
 // code example for Window Binding
+function sayWho (name) {
+    console.log(this);
+    return name;
+}
+
+sayWho("Peter-Parker"); 
 
 // Principle 2
-
+const Jax = {
+    greeting: 'Why?',
+    sayWhy: function(name) {
+        console.log(`${this.greeting} would you attack me, ${name}`);
+        console.log(this);
+    }
+};
+Jax.sayWhy('Emilio');
 // code example for Implicit Binding
 
 // Principle 3
+function PlayerLocation(aggression) {
+    this.anger = 'Who dares to enter ';
+    this.aggression = aggression;
+    this.speak = function() {
+        console.log(this.anger + this.eggression);
+        console.log(this);
+    };
+}
 
+const house = new PlayerLocation('My Home');
+const tower = new PlayerLocation('My Tower');
+const lair = new PlayerLocation('lair'); 
 // code example for New Binding
 
 // Principle 4
